abdcbd4e47dc56d53b04aa3aa7ec813c5dc7e198
Siegheart-dev <58209283+Siegheart-dev@users.noreply.github.com>
2021-12-03 12:09:33 +0200

    hand

--- main.py
+++ main.py
@@ -17,18 +17,22 @@ def start(update: Update, context: CallbackContext):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+def venue(update: Update, context: CallbackContext):
+    context.bot.send_venue(chat_id=update.effective_chat.id, latitude=46.421665, longtitude=30.726447,
+                           title="Фрэш Авто", adress="Люстдорфсая дорога, 55-а, г.Одесса, Украина")
+venue_handler = CommandHandler('venue',venue)
+dispatcher.add_handler(venue_handler)
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+#def reply(update: Update, context: CallbackContext):
+#   if update.effective_message.text == 'Где':
+#       context.bot.send_venue(chat_id=update.effective_chat.id, latitude=46.421665, longtitude=30.726447, title="Фрэш Авто",adress="Люстдорфсая дорога, 55-а, г.Одесса, Украина")
 
 
-def reply(update: Update, context: CallbackContext):
-    if update.effective_message.text == 'Где':
-       context.bot.send_venue(chat_id=update.effective_chat.id, latitude=46.421665, longtitude=30.726447, title="Фрэш Авто",adress="Люстдорфсая дорога, 55-а, г.Одесса, Украина")
 
-reply_handler = MessageHandler(Filters.text,reply)
-dispatcher.add_handler(echo_handler,reply_handler)
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN,
