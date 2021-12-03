d623feb67b1e4a588241373c63798d1ba3efe09b
Siegheart-dev <58209283+Siegheart-dev@users.noreply.github.com>
2021-12-03 15:28:28 +0200

    tired

--- main.py
+++ main.py
@@ -42,17 +42,12 @@ def echo(update: Update, context: CallbackContext):
         context.bot.send_contact(chat_id=update.effective_chat.id, phone_number=+380739401234,first_name='Фрэш Авто')
     if chat_text == 'Возврат в главное меню':
         context.bot.send_message(chat_id=update.effective_chat.id, text='Возвращаемся в главное меню...',reply_markup=main_menu)
-#def contact_handler(update: Update, context: CallbackContext):
-   # if update.effective_message.contact:
-        #id = update.effective_chat.id
-        #print(id)
-        #context.bot.forward_message(-1001780484687,update.effective_chat.id, update.effective_message)
-        #cont = str(update.message.contact.phone_number)
-        #name = str(update.message.contact.first_name)
-        #context.bot.forward_message(-1001780484687,update.message.forward_from_message_id)
-        context.bot.send_message(-1001780484687,'git')
+def contact_handler(update: Update, context: CallbackContext):
+        contact = update.effective_message.contact
+        number = contact.phone_number
+        context.bot.send_message(-1001780484687,number)
 
-#dispatcher.add_handler(MessageHandler(Filters.contact,contact_handler))
+dispatcher.add_handler(MessageHandler(Filters.contact,contact_handler))
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
