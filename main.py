4c665e087d3f75acdf231b2070c4f23c959cc6ad
Siegheart-dev <58209283+Siegheart-dev@users.noreply.github.com>
2021-12-03 14:11:40 +0200

    contact

--- main.py
+++ main.py
@@ -12,22 +12,27 @@ PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN ,use_context=True)
 # add handlers
 dispatcher = updater.dispatcher
-
-button1 = 'Прайс-лист ваших услуг'
-button2 = 'asdad'
-#keyboard_seq = [[['Прайс-лист ваших услуг',KeyboardButton]]]
-buttons = [[KeyboardButton(button1)],[KeyboardButton(button2)]]
+button1 = 'Записаться на мойку'
+button2 = 'Прайс-лист ваших услуг'
+button3 = 'Где вы находитесь?'
+button4 = 'Контактные данные ваших менеджеров'
+buttons = [[KeyboardButton(button1)],[KeyboardButton(button2)],[KeyboardButton(button3)],[KeyboardButton(button4)]]
 main_menu = telegram.ReplyKeyboardMarkup(keyboard=buttons,resize_keyboard=True)
-
+button_1 = 'Отправить номер телефона'
+button_2 = 'Возврат в главное меню'
+buttons2=[[KeyboardButton(button_1,request_contact=True)],[KeyboardButton(button_2)]]
+send_cont_menu = telegram.ReplyKeyboardMarkup(keyboard=buttons2,resize_keyboard=True)
 
 def start(update: Update, context: CallbackContext):
-
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!",reply_markup=main_menu)
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 def echo(update: Update, context: CallbackContext):
     chat_text = update.message.text
+    if chat_text == 'Записаться на мойку':
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Чтобы оставить заявку - нажмите кнопку отправить номер телефона, затем всплывет окно"
+                         " после чего нажмите 'ok' либо 'Share'", reply_markup=send_cont_menu)
     if chat_text == 'Прайс-лист ваших услуг':
         context.bot.send_document(chat_id=update.effective_chat.id, document='https://i.ibb.co/Qf2SXCM/Price-List.jpg')
     if chat_text == 'Где вы находитесь?':
@@ -35,9 +40,13 @@ def echo(update: Update, context: CallbackContext):
                                title="Фрэш Авто", address="Люстдорфсая дорога, 55-а, г.Одесса, Украина")
     if chat_text == 'Контактные данные ваших менеджеров':
         context.bot.send_contact(chat_id=update.effective_chat.id, phone_number=+380739401234,first_name='Фрэш Авто')
-    if chat_text == 'Возвращаемся в главное меню':
-        context.bot.send_message(chat_id=update.effective_chat.id, text='update.message.text')
+    if chat_text == 'Возврат в главное меню':
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Возвращаемся в главное меню...',reply_markup=main_menu)
+def contact_handler(update: Update, context: CallbackContext):
+    if update.message.contact != None:
+        context.bot.forward_message(-1001780484687,update.message)
 
+dispatcher.add_handler(MessageHandler(Filters.contact,contact_handler))
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
