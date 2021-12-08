7149763b304d147eb227d13baac67dd57a4c9ea0
Siegheart-dev <58209283+Siegheart-dev@users.noreply.github.com>
2021-12-08 15:58:07 +0200

    1

--- reference.py
+++ reference.py
@@ -4,7 +4,6 @@ from telegram.ext import Updater
 class Ref:
     def token(self):
         token = "5070280174:AAEn7rvSorr71jDZSDL9GaBzt439F-YC5Nw"
-        self.TOKEN = token
         return token
     def port(self):
         PORT = int(os.environ.get('PORT', '8443'))
@@ -13,5 +12,5 @@ class Ref:
         url = 'https://i.ibb.co/Qf2SXCM/Price-List.jpg'
         return url
     def updater_context(self):
-        updater = Updater(self.TOKEN, use_context=True)
+        updater = Updater(self.token(), use_context=True)
         return updater
